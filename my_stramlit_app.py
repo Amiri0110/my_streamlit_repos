2e1678b58abc60feb4fa62d8cfbfca337aa787c6
Venkat Amirineni <110680788+Amiri0110@users.noreply.github.com>
2022-08-05 11:43:06 -0400

    Update my_stramlit_app.py

--- my_stramlit_app.py
+++ my_stramlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('Idlie, Dosa & Poori')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.title('Build Your Own Fruit Smoothie')
