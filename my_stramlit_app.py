9d36a8966c52e0f6ffdc0bda8eceb233879cb1fd
Venkat Amirineni <110680788+Amiri0110@users.noreply.github.com>
2022-08-05 11:31:41 -0400

    Update my_stramlit_app.py

--- my_stramlit_app.py
+++ my_stramlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Breakfast Menu')
-streamlit.header('Idlie, Dosa & Poori')
+streamlit.header('Breakfast Menu')
+streamlit.text('Idlie, Dosa & Poori')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
