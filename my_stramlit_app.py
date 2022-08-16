a4a041e09724aa501e0bcf1655b8eda6d74e2d90
Venkat Amirineni <110680788+Amiri0110@users.noreply.github.com>
2022-08-16 12:45:28 -0400

    Update my_stramlit_app.py

--- my_stramlit_app.py
+++ my_stramlit_app.py
@@ -20,6 +20,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page..
 streamlit.dataframe(fruits_to_show)
 
+
 # New Section to Display the fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
@@ -33,19 +34,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-
 import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
-# Allow end userto add a fruit to the list
-#add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-#streamlit.write('Thanks for adding ', add_my_fruit)
-
-
-#my_cur.execute("insert into fruit_load_list values ('from streamlit)")
